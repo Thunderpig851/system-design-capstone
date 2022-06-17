ed7fa9bd06a97bc99c42db91d4e87574fd9ac390
CamEstep <camestep8517@gmail.com>
2022-06-17 10:25:20 -0400

    small changes

--- server/server.js
+++ server/server.js
@@ -12,7 +12,8 @@ app.get('/', (req, res) => {
 });
 
 app.get('/loaderio-118cd2c32e42d57e6c702423d9c7fe21', (req, res) => {
-  res.sendFile(path.join(__dirname, 'server', 'loaderio-118cd2c32e42d57e6c702423d9c7fe21.txt' ));
+  console.log(path.join(__dirname, 'loaderio-118cd2c32e42d57e6c702423d9c7fe21.txt'));
+  res.sendFile(path.join(__dirname, 'loaderio-118cd2c32e42d57e6c702423d9c7fe21.txt' ));
 });
 
 //Complete -sub 15ms
